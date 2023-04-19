e130cfc21902b72d2580aa1b109308c8a51e6bd2
Lennart Molnar <pernicius@web.de>
2023-04-20 00:22:13 +0200

    added a small timer display

--- projects/source/main.cpp
+++ projects/source/main.cpp
@@ -2,6 +2,8 @@
 
 #include "Parser/Lexer.h"
 
+#include <chrono>
+
 
 int main()
 {
@@ -9,8 +11,14 @@ int main()
 
 	std::string startfile = "..\\ucode_test\\main.ucode";
 
+	auto lex_start = std::chrono::high_resolution_clock::now();
 	Parser::Lexer lex;
 	result = lex.GenerateTokens(startfile);
+	auto lex_stop = std::chrono::high_resolution_clock::now();
+	auto lex_time = std::chrono::duration_cast<std::chrono::microseconds>(lex_stop - lex_start).count();
 
+	std::cout << "Done (";
+	std::cout << "Lexer: " << (lex_time/1000) << "." << (lex_time - (lex_time/1000*1000)) << "ms";
+	std::cout << ")" << std::endl;
 	return 0;
 }
