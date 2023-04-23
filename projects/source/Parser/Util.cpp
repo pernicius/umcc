c631efc752ad6a3db9938c6d9a4d831789362489
Lennart Molnar <pernicius@web.de>
2023-04-23 20:48:59 +0200

    completed PrintLine() and PrintLineWarning()

--- projects/source/Parser/Util.cpp
+++ projects/source/Parser/Util.cpp
@@ -12,7 +12,15 @@ namespace Parser {
 
 	void PrintLine(token_data& token, std::string msg)
 	{
-		// TODO: PrintLine()
+		// file:line: msg
+		std::cout << data_srcfiles[data_srclines[token.line_idx].file_idx].name << ":"
+		          << data_srclines[token.line_idx].line_number << ": "
+		          << msg << std::endl;
+
+		// source: line number
+		std::cout << "  " + std::to_string(data_srclines[token.line_idx].line_number) + " | ";
+		// source: line
+		std::cout << data_srclines[token.line_idx].line << std::endl;
 	}
 
 
@@ -53,7 +61,35 @@ namespace Parser {
 
 	bool PrintLineWarning(token_data& token, const std::string& msg)
 	{
-		// TODO: PrintLineWarning()
+		// [ERROR] file:line:pos: msg
+		std::cout << clr::red << "[WARNING] " << clr::reset
+		          << data_srcfiles[data_srclines[token.line_idx].file_idx].name << ":"
+		          << data_srclines[token.line_idx].line_number << ":"
+		          << token.pos_start << ": "
+		          << clr::cyan << msg << clr::reset << std::endl;
+
+		// source: line number
+		std::string line_num = "  " + std::to_string(data_srclines[token.line_idx].line_number) + " ";
+		std::cout << line_num << "| ";
+		// source: pre-error
+		std::cout << data_srclines[token.line_idx].line.substr(0, token.pos_start);
+		// source: error
+		std::cout << clr::cyan << data_srclines[token.line_idx].line.substr(token.pos_start, token.pos_len) << clr::reset;
+		// source: post-error
+		std::cout << data_srclines[token.line_idx].line.substr(token.pos_start + token.pos_len) << std::endl;
+
+		// marker
+		short x;
+		for (x = 0; x < line_num.length(); x++)
+			std::cout << " ";
+		std::cout << "| ";
+		for (x = 0; x < token.pos_start; x++)
+			std::cout << " ";
+		std::cout << clr::cyan << "^";
+		for (x = 0; x < token.pos_len - 1; x++)
+			std::cout << "~";
+		std::cout << clr::reset << std::endl;
+
 		return false;
 	}
 
